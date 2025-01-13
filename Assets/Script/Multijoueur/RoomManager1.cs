c84cd07ad6f7f9a32e5df3348fb970af39a98c4d
Matthieu deniau <matthieu.deniau@epita.fr>
2025-01-13 15:28:33 +0100

    full multi

--- Assets/Script/Multijoueur/RoomManager1.cs
+++ Assets/Script/Multijoueur/RoomManager1.cs
@@ -4,23 +4,41 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-/*public class RoomManager : MonoBehaviourPunCallbacks
+public class RoomManager : MonoBehaviourPunCallbacks
 {
-    public TMP_InputField roomCodeInput; // Champ pour entrer le code de la salle
-    public TMP_Text statusText; // Texte optionnel pour afficher les statuts (facultatif)
+    public TMP_InputField roomCodeInput;
+    public TMP_Text statusText;
 
     public GameObject playerPrefab;
-  
-    [Space]
-    public Transform spawnPoint1;
-    public Transform spawnPoint2;
+    
+    public void Start()
+    {
+        statusText.text = "Connecting...";
+        Debug.Log("Connecting...");
 
-    public void JoinOrCreateRoom()
+        if (!PhotonNetwork.IsConnected)
+        {
+            statusText.text = "Connexion à Photon en cours...";
+            //Debug.LogWarning("Le client n'est pas encore prêt. Veuillez attendre la connexion au Master Server.");
+            PhotonNetwork.ConnectUsingSettings();
+            //return;
+        }
+    }
+    
+    public override void OnConnectedToMaster()
+    {
+        statusText.text = "You are connected to the Server";
+        Debug.Log("Connected");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public void JoinCreateRoom()
     {
         if (!PhotonNetwork.IsConnectedAndReady)
         {
-            statusText.text = "Connexion à Photon en cours...";
-            Debug.LogWarning("Le client n'est pas encore prêt. Veuillez attendre la connexion au Master Server.");
+            statusText.text = "Connecting to server...";
+            Debug.LogWarning("The client is not ready yet. Please wait for the connection to the Master Server.");
+            PhotonNetwork.ConnectUsingSettings();
             return;
         }
 
@@ -28,63 +46,27 @@ using TMPro;
 
         if (!string.IsNullOrEmpty(roomCode))
         {
-            statusText.text = "Tentative de connexion à la salle...";
-            PhotonNetwork.JoinRoom(roomCode); // Tente de rejoindre la salle
+            statusText.text = "Trying to connect to the room...";
+            Debug.Log("We are in the Lobby");
+
+            RoomOptions roomOptions = new RoomOptions();
+            roomOptions.MaxPlayers = 2;
+            roomOptions.IsVisible = true;
+            roomOptions.IsOpen = true;
+
+            TypedLobby typedLobby = new TypedLobby("DefaultLobby", LobbyType.Default);
+
+            PhotonNetwork.JoinOrCreateRoom(roomCode, roomOptions, typedLobby);
         }
         else
         {
-            statusText.text = "Le code de la salle est vide !";
+            statusText.text = "The room code is empty!";
+            return;
         }
-    }
-
-    public override void OnConnectedToMaster()
-    {
-        Debug.Log("Connecté au Master Server !");
-        statusText.text = "Connecté. Prêt à rejoindre ou créer une salle.";
     }
 
     public override void OnJoinedRoom()
     {
-        PhotonNetwork.LoadLevel("Map");
-        
-        // Créer dynamiquement deux points de spawn
-        GameObject spawn1 = new GameObject("SpawnPoint1");
-        spawn1.transform.position = new Vector3(-3, 2, 2);
-        spawnPoint1 = spawn1.transform;
-
-        GameObject spawn2 = new GameObject("SpawnPoint2");
-        spawn2.transform.position = new Vector3(2, 2, 2);
-        spawnPoint2 = spawn2.transform;
-        
-        
-        Debug.Log("Rejoint la salle avec succès !");
-        statusText.text = "Rejoint la salle : " + PhotonNetwork.CurrentRoom.Name;
-
-        //GameObject _player = PhotonNetwork.Instantiate(player.name, SpawnPoint.position, Quaternion.identity);
-        
-        int playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
-
-        Transform spawnPoint = playerCount == 1 ? spawnPoint1 : spawnPoint2;
-
-        GameObject _player = PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, Quaternion.identity);
-
-        _player.GetComponent<PlayerManager>().IslocalPlayer();
+        PhotonNetwork.LoadLevel("Map_Multi");
     }
-
-    public override void OnJoinRoomFailed(short returnCode, string message)
-    {
-        Debug.Log($"Salle introuvable, création d'une nouvelle salle : {message}");
-        statusText.text = "Salle introuvable. Création d'une nouvelle salle...";
-
-        // Si la salle n'existe pas, crée une nouvelle salle
-        string roomCode = roomCodeInput.text;
-        RoomOptions roomOptions = new RoomOptions { MaxPlayers = 4 }; // Configurer les options de la salle
-        PhotonNetwork.CreateRoom(roomCode, roomOptions); // Crée la salle
-    }
-
-    public override void OnCreatedRoom()
-    {
-        Debug.Log("Salle créée avec succès !");
-        statusText.text = "Salle créée avec succès ! En attente des autres joueurs...";
-    }
-}*/
+}
--- Assets/Script/Multijoueur/RoomManager2.cs
+++ Assets/Script/Multijoueur/RoomManager2.cs
@@ -1,4 +1,4 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
@@ -6,10 +6,26 @@ using Photon.Realtime;
 
 public class RoomManager2 : MonoBehaviour
 {
-    
+    public GameObject playerPrefab;
+    [Space]
+    public Transform spawnPoint1;
+    public Transform spawnPoint2;
 
-    public void Starting()
+    public void Start()
     {
-        
+        int playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
+        Debug.Log("Player Count: " + playerCount);
+            
+        Transform spawnPoint = playerCount == 1 ? spawnPoint1 : spawnPoint2;
+
+        if (spawnPoint == null)
+        {
+            Debug.LogError("Spawn point is not assigned!");
+            return;
+        }
+            
+        GameObject _player = PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, Quaternion.identity);
+        _player.GetComponent<PlayerSetup>()?.IslocalPlayer();
+        Debug.Log("Player instantiated at: " + spawnPoint.position);
     }
-}*/
+}
